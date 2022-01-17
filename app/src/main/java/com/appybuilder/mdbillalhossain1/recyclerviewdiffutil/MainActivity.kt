44f2b2a602fefb9322af92bfa4985cb67faa4812
masum <masum.abir43@gmail.com>
2022-01-17 22:23:21 +0600

    diff utils used to sorted list

--- app/src/main/java/com/appybuilder/mdbillalhossain1/recyclerviewdiffutil/MainActivity.kt
+++ app/src/main/java/com/appybuilder/mdbillalhossain1/recyclerviewdiffutil/MainActivity.kt
@@ -1,11 +1,49 @@
 package com.appybuilder.mdbillalhossain1.recyclerviewdiffutil
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.appybuilder.mdbillalhossain1.recyclerviewdiffutil.databinding.ActivityMainBinding
+import com.appybuilder.mdbillalhossain1.recyclerviewdiffutil.utils.DummyEmployeeDataUtils
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val mRecyclerViewAdapter by lazy {
+        EmployeeRecyclerViewAdapter(DummyEmployeeDataUtils.employeeListSortedByRole)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = mRecyclerViewAdapter
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.sort_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.sort_by_name -> {
+                mRecyclerViewAdapter.updateEmployeeListItems(
+                    DummyEmployeeDataUtils.employeeListSortedByName
+                )
+                return true
+            }
+            R.id.sort_by_role -> {
+                mRecyclerViewAdapter.updateEmployeeListItems(
+                    DummyEmployeeDataUtils.employeeListSortedByRole
+                )
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
---
+++ app/src/main/java/com/appybuilder/mdbillalhossain1/recyclerviewdiffutil/utils/DummyEmployeeDataUtils.kt
@@ -0,0 +1,37 @@
+package com.appybuilder.mdbillalhossain1.recyclerviewdiffutil.utils
+
+import com.appybuilder.mdbillalhossain1.recyclerviewdiffutil.Employee
+import java.util.*
+
+object DummyEmployeeDataUtils {
+    val employeeListSortedByName: List<Employee>
+        get() {
+            val employeeList: List<Employee> = employeeList
+            Collections.sort(
+                employeeList,
+                Comparator<Employee> { a1, a2 -> a1.getName().compareTo(a2.getName()) })
+            return employeeList
+        }
+    val employeeListSortedByRole: List<Employee>
+        get() {
+            val employeeList: List<Employee> = employeeList
+            Collections.sort(
+                employeeList,
+                Comparator<Employee> { a1, a2 -> a2.getRole().compareTo(a1.getRole()) })
+            return employeeList
+        }
+    val employeeList: List<Employee>
+        get() {
+            val employees: MutableList<Employee> = ArrayList<Employee>()
+            employees.add(Employee(1, "Employee 1", "Developer"))
+            employees.add(Employee(2, "Employee 2", "Tester"))
+            employees.add(Employee(3, "Employee 3", "Support"))
+            employees.add(Employee(4, "Employee 4", "Sales Manager"))
+            employees.add(Employee(5, "Employee 5", "Manager"))
+            employees.add(Employee(6, "Employee 6", "Team lead"))
+            employees.add(Employee(7, "Employee 7", "Scrum Master"))
+            employees.add(Employee(8, "Employee 8", "Sr. Tester"))
+            employees.add(Employee(9, "Employee 9", "Sr. Developer"))
+            return employees
+        }
+}
